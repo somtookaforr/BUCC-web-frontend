f069c22912cd6404be044e1ac7550a595ec15b18
Boye-dev <oyelolaboye@gmail.com>
2022-01-31 21:41:54 +0100

    events page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import Index from './Pages/index'
-import './App.css';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Index from "./Pages/index";
+import Events from "./Pages/Events";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Import your individual pages here
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      {/* Call them here */}
-      <Switch>
-        <Route exact path="/" component={Index}/> 
-      </Switch>
-    </div>
+      <div className="App">
+        {/* Call them here */}
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/events" component={Events} />
+        </Switch>
+      </div>
     </Router>
-
   );
 }
 
